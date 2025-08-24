dcd5d2319cd638e9284b1c2b01f20d4194ea91c0
HRITIK18711 <hritikupadhyay2002@gmail.com>
2025-08-24 22:48:06 +0530

    code changed

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function MainContent() {
         <>
           <Card_Header />
           <Slider />
-          {/* <Mobile_Component /> */}
+          <Mobile_Component />
           {/* <Electronics />
           <Tv_Appliance />
           <Furniture /> */}
